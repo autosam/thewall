9491bda590050105930b9d67038074be55c94c68
autosam <autosam.sm@gmail.com>
2024-08-31 23:56:22 +0330

    make all cards white

--- components/TalkCardsContainer/TalkCardsContainer.tsx
+++ components/TalkCardsContainer/TalkCardsContainer.tsx
@@ -40,7 +40,7 @@ export const TalkCardsContainer = (props: TalkCardsContainerProps) => {
           {cardsCol.map((card: TalkData, ci: number) => {
             let dark = false;
             if (ci % 3 == i % 2) dark = true;
-            return <TalkCard dark={dark} key={uid()} {...card} />;
+            return <TalkCard dark={false} key={uid()} {...card} />;
           })}
         </div>
       ))}
